b6fc254499700012939cdb08b159351fcfe3d81f
oliseh6123 <oliseh.obiajuru@gmail.com>
2024-08-28 21:38:00 +0100

    arrays and custom types

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,19 +2,33 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract SimpleStorage{
-    uint256 public favoriteNumber;
+    uint256 public myFavoriteNumber;
 
     //uint256 [] listOfFavoriteNumber;
     
     //In solidity you can create a type keyword by using the word struct
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+    // Person public james = Person({favoriteNumber: 23, name: "James"});
+    // Person public nkiru = Person({favoriteNumber: 37, name: "Nkiru"});
+    
+    Person [] public listOfPeople;
 
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     function retrieve() public view returns (uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+
+    function addPerson (string memory _name, uint256 _favoriteNmuber) public {
+        listOfPeople.push(Person(_favoriteNmuber, _name));
     }
 }
 
