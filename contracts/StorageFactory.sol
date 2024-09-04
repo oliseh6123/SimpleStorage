7c111c437eba2248b8b0e2c08a4a0d2aac0f8288
oliseh6123 <oliseh.obiajuru@gmail.com>
2024-09-04 13:04:43 +0100

    Deploying a Contract from a contract

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0; 
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage public simpleStorage;
+
+    function createSimpleStorageContract() public{
+        simpleStorage = new SimpleStorage();
+
+    }
+
+}
