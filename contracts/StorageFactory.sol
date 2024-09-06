3fde1ca3e777aa63bda4bdcd2c8196ca966378cd
oliseh6123 <oliseh.obiajuru@gmail.com>
2024-09-06 08:44:30 +0100

    Application Binary Interface

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -8,11 +8,22 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContract;
 
     function createSimpleStorageContract() public{
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContract.push(newSimpleStorageContract);
 
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
     }
 
 }
